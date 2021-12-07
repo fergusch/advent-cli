353e7ec3eeb0c493feb32220ff5e66c8eec6451c
fergusch <27470183+fergusch@users.noreply.github.com>
2021-12-06 19:17:30 -0600

    use if/else instead of try/except

--- advent_cli/config.py
+++ advent_cli/config.py
@@ -3,14 +3,14 @@ import sys
 
 from .utils import colored
 
-try:
+if 'ADVENT_SESSION_COOKIE' in os.environ:
     session_cookie = os.environ['ADVENT_SESSION_COOKIE']
-except KeyError:
+else:
     print(colored('Session cookie not set.', 'red'))
     print(colored('Grab your AoC session cookie from a browser and store it in an environment variable ADVENT_SESSION_COOKIE.', 'red'))
     sys.exit(1)
 
-try:
+if 'ADVENT_PRIV_BOARDS' in os.environ:
     private_leaderboards = os.environ['ADVENT_PRIV_BOARDS'].split(',')
-except KeyError:
+else:
     private_leaderboards = []
