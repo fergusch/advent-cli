5cef4bca7b9246f3d55e38692fff857599ff9c86
fergusch <27470183+fergusch@users.noreply.github.com>
2021-12-09 16:53:36 -0600

    clean up test_get

--- tests/commands/test_get.py
+++ tests/commands/test_get.py
@@ -3,8 +3,6 @@ from mock import patch, call, mock_open, MagicMock
 from advent_cli import commands
 
 
-# ---- test get() ----
-
 @patch('os.mkdir')
 @patch('builtins.open', new_callable=mock_open())
 @patch('requests.get')
@@ -22,18 +20,20 @@ def test_get(mock_get, mock_open, mock_mkdir):
     commands.get('2099', '99')
     mock_open.assert_has_calls([
         call('2099/99/prompt.md', 'w'),
-        call().__enter__().write(
-            '\nDay 1: Test\n-----------\n\n\nThis is a test puzzle.\n\n\n'),
         call('2099/99/input.txt', 'w'),
-        call().__enter__().write('0,1,2,3,4'),
         call('2099/99/example_input.txt', 'w'),
-        call().close(),
         call('2099/99/solution.py', 'w'),
+    ], any_order=True)
+    mock_open.assert_has_calls([
         call().__enter__().write(
-            '## advent of code 2099\n## https://adventofcode.com/2099\n' +
-            '## day 99\n\ndef parse_input(lines):\n    pass\n\n' +
-            'def part1(data):\n    pass\n\ndef part2(data):\n    pass'
+            '\nDay 1: Test\n-----------\n\n\nThis is a test puzzle.\n\n\n'
         ),
+        call().__enter__().write('0,1,2,3,4'),
+        call().__enter__().write(
+            ('## advent of code 2099\n## https://adventofcode.com/2099\n'
+             '## day 99\n\ndef parse_input(lines):\n    pass\n\n'
+             'def part1(data):\n    pass\n\ndef part2(data):\n    pass')
+        )
     ], any_order=True)
 
 
@@ -49,8 +49,8 @@ def test_get_path_exists(mock_exists, mock_open):
 @patch('requests.get')
 def test_get_puzzle_locked(mock_get, mock_open):
     mock_get.return_value.status_code = 404
-    mock_get.return_value.text = 'Please don\'t repeatedly request ' + \
-                                 'this endpoint before it unlocks!'
+    mock_get.return_value.text = ('Please don\'t repeatedly request '
+                                  'this endpoint before it unlocks!')
     commands.get('2099', '99')
     mock_open.assert_not_called()
 
